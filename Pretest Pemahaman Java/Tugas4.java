d54e61f24e727b6aa08191fd14214cf7dc697ca0
WahyuAwwaluddin <WahyuAwwaluddin@users.noreply.github.com>
2017-02-27 12:28:34 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas4.java
@@ -0,0 +1,13 @@
+public class Tugas4{
+
+     public static void main(String []args){
+        int a = 1;
+        int hasil = Penjumlahan(a);
+        System.out.println("Angka penjumlahan " + a + " + 2 = " + hasil);
+     }
+     
+     public static int Penjumlahan(int a1) {
+      int angka = a1;
+      return angka + 2; 
+   }
+}
