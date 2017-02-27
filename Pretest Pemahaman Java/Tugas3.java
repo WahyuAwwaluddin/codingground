885a9267cb44f45b5670c288afd25555e35d71c1
WahyuAwwaluddin <WahyuAwwaluddin@users.noreply.github.com>
2017-02-27 12:28:33 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas3.java
@@ -0,0 +1,13 @@
+public class Tugas3{
+
+     public static void main(String []args){
+        int a = 3;
+        int b = TampilAngka(a);
+        System.out.println("Angka yang anda masukkan : " + b);
+     }
+     
+     public static int TampilAngka(int a1) {
+      int angka = a1;
+      return angka; 
+   }
+}
